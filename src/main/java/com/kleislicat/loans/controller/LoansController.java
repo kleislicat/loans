90e29002cb5806388c319a61b5843a60925f2590
kleislicat <79645287+kleislicat@users.noreply.github.com>
2026-01-04 16:20:06 +0000

    feat: Implement Edge Server/API Gateway

--- src/main/java/com/kleislicat/loans/controller/LoansController.java
+++ src/main/java/com/kleislicat/loans/controller/LoansController.java
@@ -14,7 +14,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +41,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
 public class LoansController {
+
+  private static final Logger logger = LoggerFactory.getLogger(LoansController.class);
 
   private ILoansService iLoansService;
 
@@ -102,9 +106,12 @@ public class LoansController {
   }
   )
   @GetMapping("/fetch")
-  public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam
-  @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
-  String mobileNumber) {
+  public ResponseEntity<LoansDto> fetchLoanDetails(
+      @RequestHeader("eazybank-correlation-id") String correlationId,
+      @RequestParam
+      @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+      String mobileNumber) {
+    logger.debug("eazyBank-correlation-id found: {} ", correlationId);
     LoansDto loansDto = iLoansService.fetchLoan(mobileNumber);
     return ResponseEntity.status(HttpStatus.OK).body(loansDto);
   }
